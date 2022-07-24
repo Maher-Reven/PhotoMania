725725126baaa9f71d861bdc2580b3b7a813b3e2
Maher-rv <maher@revenyou.io>
2022-07-24 21:51:04 +0200

    Initial Commit

---
+++ photoMania/photoManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  photoManiaApp.swift
+//  photoMania
+//
+//  Created by Maher on 24/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct photoManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
